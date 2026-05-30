7bab5834e9548c8edd5ef745308cc18e66996885
n.vershinin <n.vershinin.job@gmail.com>
2026-05-30 18:21:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Привет! Это твое первое Go-приложение в Docker!")
+		fmt.Fprintf(w, "Это мое первое Go-приложение в Docker!")
 	})
 	fmt.Println("Сервер запущен на порту 8080...")
 	http.ListenAndServe(":8080", nil)
